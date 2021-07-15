bad3077cce9aced7f2c2432b0ce4493e4c99113a
Tuan Anh <manhhungit.37@gmail.com>
2021-07-15 17:23:35 +0700

    add uuid

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const jsonServer = require('json-server')
+const { v4: uuidv4 } = require('uuid');
+
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
@@ -17,6 +19,7 @@ server.get('/echo', (req, res) => {
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
+    req.body.id = uuidv4();
     req.body.createdAt = Date.now()
     req.body.updatedAt = Date.now()
   }
